be00ee03b9bdea543b51c85df1ffa4e33278b2c6
Alif <79266506+S-Alif@users.noreply.github.com>
2023-06-14 20:00:20 +0600

    update: upadate js according to nav-menu design fix

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+// get the elements
+let openIcon = document.querySelector('.bar')
+let closeIcon = document.querySelector('.cross')
+let navitem = document.querySelector('.nav-item-wrapper')
+
 // nav toggle on mobile
 document.querySelector('.control-btn').addEventListener('click', () => {
-  let openIcon = document.querySelector('.bar')
-  let closeIcon = document.querySelector('.cross')
-  let navitem = document.querySelector('.nav-item-wrapper')
-
   if(openIcon.classList.contains('open')){
     openIcon.classList.remove("open")
     closeIcon.classList.add('open')
@@ -14,4 +15,13 @@ document.querySelector('.control-btn').addEventListener('click', () => {
     closeIcon.classList.remove('open')
     navitem.classList.remove('opened')
   }
+})
+
+// close nav on link click on mobile
+document.querySelectorAll('.navbar .nav-item-wrapper .nav-links').forEach(e => {
+  e.addEventListener('click', () => {
+    openIcon.classList.add("open")
+    closeIcon.classList.remove('open')
+    navitem.classList.remove('opened')
+  })
 })
